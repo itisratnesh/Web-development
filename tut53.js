b12f8073ff3a2d30a9fb01c58f3be2fd5853dc2c
Ratnesh Tripathi <150932257+r01tripathi@users.noreply.github.com>
2023-12-09 18:23:36 +0530

    update

--- tut53.js
+++ tut53.js
@@ -1,1 +1,10 @@
 console.log("This is tutorial 53");
+
+let name0 = "Ratnesh";
+let name1 = "K";
+let name2 = "Tripathi";
+let name3 = "Ratnesh";
+console.log(name0 + " is a good person");
+console.log(name1 + " is a good person");
+console.log(name2 + " is a good person");
+console.log(name3 + " is a good person");
